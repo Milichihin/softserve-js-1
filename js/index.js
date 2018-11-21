0262dd64e5ab41b7e2d6069c2a147c0e66f5bae0
Milichihin <32966900+Milichihin@users.noreply.github.com>
2018-11-21 17:18:21 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const greetings = "Let's study!";
 alert(greetings);
 
-const coefficientA = parseInt(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"a\" and insert this one into the cell below"));
-const coefficientB = parseInt(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"b\" and insert this one into the cell below"));
-const coefficientC = parseInt(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"c\" and insert this one into the cell below"));
+const coefficientA = parseFloat(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"a\" and insert this one into the cell below"));
+const coefficientB = parseFloat(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"b\" and insert this one into the cell below"));
+const coefficientC = parseFloat(prompt("ax2 + bx + c = 0\nTo solve the quadratic equation above please choose an any number for the coefficient \"c\" and insert this one into the cell below"));
 
 
 if (Number.isNaN(coefficientA)) {
@@ -17,7 +17,7 @@ if (Number.isNaN(coefficientC)) {
 }
 
 const discriminant = solveDisc (coefficientA, coefficientB, coefficientC);
-alert ("solve of the discriminant D = " + discriminant)
+alert ("solve of the discriminant = " + discriminant)
 function solveDisc (a, b, c) {
     return b*b-4*a*c;
 }
@@ -26,18 +26,17 @@ if (discriminant < 0) {
     alert ("There are no real roots.")
 } else if (discriminant === 0) {
     alert ("There is one real root.")
-} else if (discriminant > 0) {
-    alert ("There are two real roots.")
-}
+} else alert ("There are two real roots.") // not necessary to insert one more condition because third condition will definitely ">0"
+
 
 const x1 = realRoot1(coefficientA, coefficientB, discriminant);
 alert("solve of the first root X1 = " + x1)
 function realRoot1(a, b, d) {
-    return (-b + Math.sqrt(d)) / 2*a;
+    return (-b + Math.sqrt(d)) / (2*a);
 }
 
 const x2 = realRoot2(coefficientA, coefficientB, discriminant);
 alert("solve of the second root X2 = " + x2)
 function realRoot2(a, b, d) {
-    return (-b - Math.sqrt(d)) / 2*a;
+    return (-b - Math.sqrt(d)) / (2*a);
 }
